047eea7f970e54f20d46115bbca335a500f2f4d0
ehdgusdl9177 <k23129177@gmail.com>
2023-10-25 11:36:00 +0900

    useQuery Variables

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieid: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie detail</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(data);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
